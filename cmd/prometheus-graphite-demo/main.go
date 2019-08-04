c30dfea5a860b0e41d90c714a59607c2b46f2b45
Martins Irbe <martins_irbe@mail.com>
2019-08-04 20:01:25 +0100

    Make graphite URL configurable

--- cmd/prometheus-graphite-demo/main.go
+++ cmd/prometheus-graphite-demo/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"net/http"
+	"os"
 	"time"
 
+	cli "github.com/jawher/mow.cli"
 	"github.com/martinsirbe/prometheus-graphite-demo/internal/db/postgres"
 	"github.com/martinsirbe/prometheus-graphite-demo/internal/service"
 	"github.com/prometheus/client_golang/prometheus/graphite"
@@ -13,35 +15,55 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	appName        = "prometheus-graphite-demo"
+	appDescription = "An example app which exposes Prometheus metrics on /metrics endpoint and also sends metrics " +
+		"to Graphite using Prometheus Graphite Bridge."
+)
+
 func main() {
-	storageClient := postgres.NewInstrumentedClientPrometheus()
-	http.Handle("/metrics", promhttp.Handler())
+	app := cli.App(appName, appDescription)
 
-	// Example for other clients, such as Graphite, DataDog where the client post metrics
-	//storageClient := postgres.InstrumentedClientOther()
-
-	ll := service.NewLogicLayer(storageClient)
-	go func() {
-		if err := ll.Run(); err != nil {
-			log.WithError(err).Fatal("failed to run")
-		}
-	}()
-
-	bridge, err := graphite.NewBridge(&graphite.Config{
-		URL:           "localhost:2003",
-		Prefix:        "poc",
-		Timeout:       5 * time.Second,
-		ErrorHandling: graphite.AbortOnError,
-		Logger:        log.StandardLogger(),
+	carbonURL := app.String(cli.StringOpt{
+		Name:   "carbon-url",
+		Desc:   "Carbon URL for sending metrics to Graphite, format <host>:<port>.",
+		EnvVar: "CARBON_URL",
 	})
 
-	if err != nil {
-		logrus.WithError(err).Fatal("failed to create Prometheus Graphite bridge")
+	app.Action = func() {
+		storageClient := postgres.NewInstrumentedClientPrometheus()
+		http.Handle("/metrics", promhttp.Handler())
+
+		// Example for other clients, such as Graphite, DataDog where the client post metrics
+		//storageClient := postgres.InstrumentedClientOther()
+
+		ll := service.NewLogicLayer(storageClient)
+		go func() {
+			if err := ll.Run(); err != nil {
+				log.WithError(err).Fatal("failed to run")
+			}
+		}()
+
+		bridge, err := graphite.NewBridge(&graphite.Config{
+			URL:           *carbonURL,
+			Prefix:        "poc",
+			Timeout:       5 * time.Second,
+			ErrorHandling: graphite.AbortOnError,
+			Logger:        log.StandardLogger(),
+		})
+
+		if err != nil {
+			logrus.WithError(err).Fatal("failed to create Prometheus Graphite bridge")
+		}
+
+		go bridge.Run(context.Background())
+
+		if err := http.ListenAndServe(":1337", nil); err != nil {
+			log.WithError(err).Fatal("failed to listen and serve metrics")
+		}
 	}
 
-	go bridge.Run(context.Background())
-
-	if err := http.ListenAndServe(":1337", nil); err != nil {
-		log.WithError(err).Fatal("failed to listen and serve metrics")
+	if err := app.Run(os.Args); err != nil {
+		log.WithError(err).Panicf("app failed to run")
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/gofrs/uuid v3.2.0+incompatible
 	github.com/golang/mock v1.3.1
+	github.com/jawher/mow.cli v1.1.0
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pkg/errors v0.8.1
